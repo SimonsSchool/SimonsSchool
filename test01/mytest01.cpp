d4300528aba815e0ff0bd10738cc0a0e6cb57f98
Andrey E. Antipov <Andrey.E.Antipov@gmail.com>
2016-06-19 12:24:31 -0400

    something is broken

--- test01/mytest01.cpp
+++ test01/mytest01.cpp
@@ -5,7 +5,7 @@ using namespace myexample;
 
 int main() 
 {
-    if (get_three() != 3) return 1;
+    if (get_three() != 4) return 1;
     if (get_three() == 3) { std::cout << "3" << std::endl; } else return 1;
     return 0;
 }
